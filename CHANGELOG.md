913e5ef0426825a948c608483330af9c245be236
jczzq <1195262383@qq.com>
2019-02-16 14:07:09 +0800

    add: mone.genarate

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,21 @@
 # Change Log
 All notable changes to the "mone" extension will be documented in this file.
+指导原则
+- 记住日志是写给人的，而非机器。
+- 每个版本都应该有独立的入口。
+- 同类改动应该分组放置。
+- 版本与章节应该相互对应。
+- 新版本在前，旧版本在后。
+- 应包括每个版本的发布日期。
+- 注明是否遵守语义化版本格式.
 
+变动类型
+- Added 新添加的功能。
+- Changed 对现有功能的变更。
+- Deprecated 已经不建议使用，准备很快移除的功能。
+- Removed 已经移除的功能。
+- Fixed 对bug的修复
+- Security 对安全的改进
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
 ## [Unreleased]
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,33 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// 激活扩展时调用此方法
+// 您的扩展将在命令第一次执行时被激活
 export function activate(context: vscode.ExtensionContext) {
+  // 使用控制台输出诊断信息(console.log)和错误(console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('mone is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "mone" is now active!');
+  // helloWorld
+  let disposable = vscode.commands.registerCommand(
+    "extension.helloWorld",
+    () => {
+      // 在这里放置的代码将在每次执行命令时执行
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello World mone!");
+    }
+  );
+  context.subscriptions.push(disposable);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+  // 生成代码
+  let genarate = vscode.commands.registerCommand(
+    "extension.genarate",
+    () => {
+      
+      vscode.window.showInformationMessage("Hello World mone!");
+    }
+  );
+  context.subscriptions.push(genarate);
 }
 
 // this method is called when your extension is deactivated
